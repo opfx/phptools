20bedffe11d3ebe4f483071df889ab40b5e3b313
opfx <opfx@opfx.org>
2021-04-06 12:15:33 -0400

    wip (implementation): add output directory creation for `phpab`

--- packages/autoload/src/bin/phpab.ts
+++ packages/autoload/src/bin/phpab.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import * as proc from 'child_process';
+import * as File from 'fs';
 import * as Path from 'path';
 
 import * as tool from './../constants';
@@ -11,6 +12,20 @@ spawnArgs.unshift(toolPath);
 
 // turn on the ability to write to a phar archive
 spawnArgs.unshift('-d phar.readonly=Off');
+
+let output;
+spawnArgs.forEach((arg, index) => {
+  if (arg === '-o' || arg === '--output') {
+    output = spawnArgs[index + 1];
+  }
+});
+if (output) {
+  output = Path.resolve(output);
+  output = output.replace(Path.basename(output), '');
+  if (!File.existsSync(output)) {
+    File.mkdirSync(output, { recursive: true });
+  }
+}
 
 const child = proc.spawn('php', spawnArgs, { stdio: 'inherit', windowsHide: true });
 child.on('close', (code) => {
